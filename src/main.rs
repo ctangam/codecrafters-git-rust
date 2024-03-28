30661679e69b0208f9fc74caf59329a23a1f247d
汤淳 <tangchun@szcgc.com>
2024-03-28 10:41:51 +0800

    pass 1st stage

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,31 @@ use std::env;
 #[allow(unused_imports)]
 use std::fs;
 
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Parser)]
+enum Command {
+    Init,
+
+}
+
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+    
+    let args = Args::parse();
+    match args.command {
+        Command::Init => {
+            fs::create_dir(".git").unwrap();
+            fs::create_dir(".git/objects").unwrap();
+            fs::create_dir(".git/refs").unwrap();
+            fs::write(".git/HEAD", "refs/heads/main\n").unwrap();
+        }
+    }
 
     // Uncomment this block to pass the first stage
     // let args: Vec<String> = env::args().collect();
